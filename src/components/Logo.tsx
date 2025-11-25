4e0cc4a8a51a4482049f849db6af23a2b4df3d1f
Teckdegen <teckdegen@example.com>
2025-11-25 01:04:07 +0100

    Update favicon and logo to use local icon.png instead of Twitter image URL

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ interface LogoProps {
 export default function Logo({ size = 40, className = '' }: LogoProps) {
   return (
     <img 
-      src="https://pbs.twimg.com/profile_images/1971138911138152448/skxW4GqN_400x400.jpg" 
+      src="/icon.png" 
       alt="KARDS Logo" 
       className={`rounded-2xl object-cover ${className}`}
       style={{ width: size, height: size }}
